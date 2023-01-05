62c121d6bbfc941103c9c82b0f324aefdc2fe23a
Sathish0101 <122032665+Sathish0101@users.noreply.github.com>
2023-01-05 21:02:49 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void SieveOfEratosthenes(int n)
+{	
+	//int prime[100]={1};->works
+	//int prime[n]={1};->doesnot work
+	int prime[n];
+	for(int i=0;i<n;i++)
+		prime[i]=1;
+	prime[0]=0;
+	
+	for (int p = 2; p * p <= n; p++) 
+	{
+		if (prime[p-1] == 1) 
+		{
+			// Numbers before pXp have already been crossed
+			for (int i = p * p; i <= n; i += p)
+	 			prime[i-1] = 0;
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(prime[i]==1)
+			printf("%d ",(i+1));
+	}
+}
+
+void main()
+{
+	printf("Enter the number upto which all the prime numbers are to be printed:");
+	int n;
+	scanf("%d",&n);
+	printf("The prime numbers upto %d are:\n", n);
+	SieveOfEratosthenes(n);
+}
+Footer
